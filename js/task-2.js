f46dc65eb2f785d0643ca04b2413670043455a83
Oleh <62122094+olehsolonin@users.noreply.github.com>
2024-04-14 20:24:10 +0300

    Initial commit

---
+++ js/task-2.js
@@ -0,0 +1,33 @@
+class Storage {
+	#items;
+
+
+	constructor(params) {
+		this.#items = params;
+	}
+
+	getItems() {
+		return this.#items;
+	}
+
+	addItem(newItem) {
+		this.#items.push(newItem);
+	}
+
+	removeItem(itemToRemove) {
+		this.#items = this.#items.filter(function (item) {
+			return item !== itemToRemove;
+		});
+	}
+
+
+
+}
+
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
